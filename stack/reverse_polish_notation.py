fa03162623dae1bcf65ea6a2f1fb147b73c638f5
chrisrossell <chrisrossell1999@gmail.com>
2025-11-17 22:46:51 +0000

    reverse polish notation solutions

--- stack/reverse_polish_notation.py
+++ stack/reverse_polish_notation.py
@@ -1,15 +1,22 @@
 from typing import List
 
 
-class NotCompleteSolution:
+class MySolution:
     def evalRPN(self, tokens: List[str]) -> int:
-        if len(tokens) < 3:
-            return 0
+        print(tokens)
+        if len(tokens) == 1:
+            return int(tokens[0])
 
-        # Need to make this smarter and search for an operand preceded by two numbers
-        operand_1 = int(tokens[0])
-        operand_2 = int(tokens[1])
-        operator = tokens[2]
+        operators = set(["+", "-", "*", "/"])
+
+        operator_index = 0
+        for i in range(len(tokens)):
+            if tokens[i] in operators:
+                operator_index = i
+                break
+        operand_1 = int(tokens[operator_index - 2])
+        operand_2 = int(tokens[operator_index - 1])
+        operator = tokens[operator_index]
         result = 0
         match operator:
             case "+":
@@ -19,14 +26,66 @@ class NotCompleteSolution:
             case "*":
                 result = operand_1 * operand_2
             case "/":
-                result = operand_1 // operand_2
+                result = int(operand_1 / operand_2)
 
         if len(tokens) == 3:
             return result
         else:
-            tokens.pop(0)
-            tokens.pop(0)
-            tokens.pop(0)
-            tokens.insert(0, result)
+            tokens.pop(operator_index - 2)
+            tokens.pop(operator_index - 2)
+            tokens.pop(operator_index - 2)
+            tokens.insert(operator_index - 2, str(result))
             return self.evalRPN(tokens)
+
+# this is O(n^2) worse case time complexity if the operators are all at the end of the sequence, as for each
+# operation we loop through the whole array
+# this is O(n) space complexity for the input array
+
+class Recursion:
+    def evalRPN(self, tokens: List[str]) -> int:
+        def solveRecursivelyByPoppingFromTheRight():
+            popped = tokens.pop()
+            print(tokens)
+            print(f"popped {popped}")
+            if popped not in ["/", "*", "+", "-"]:
+                return int(popped)
+
+            right_operand = solveRecursivelyByPoppingFromTheRight()
+            left_operand = solveRecursivelyByPoppingFromTheRight()
+
+            print(left_operand, right_operand)
+
+            match popped:
+                case "+":
+                    return left_operand + right_operand
+                case "-":
+                    return left_operand - right_operand
+                case "*":
+                    return left_operand * right_operand
+                case "/":
+                    return int(left_operand / right_operand)
+
+        return solveRecursivelyByPoppingFromTheRight()
+
+class Stack:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in range(len(tokens)):
+            print(tokens)
+            print(stack)
+            if tokens[i] in "+-/*":
+                right_operand = stack.pop()
+                left_operand = stack.pop()
+                match tokens[i]:
+                    case "+":
+                        stack.append(left_operand + right_operand)
+                    case "-":
+                        stack.append(left_operand - right_operand)
+                    case "*":
+                        stack.append(left_operand * right_operand)
+                    case "/":
+                        stack.append(int(left_operand / right_operand))
+                continue
+            stack.append(int(tokens[i]))
+        return stack[0]
 
