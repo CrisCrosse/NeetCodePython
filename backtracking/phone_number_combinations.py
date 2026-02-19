0177fd43e29a522c7db12bde383af81cae6b3053
chrisrossell <chrisrossell1999@gmail.com>
2026-02-19 08:33:13 +0000

    iteration phone number combination solution

--- backtracking/phone_number_combinations.py
+++ backtracking/phone_number_combinations.py
@@ -24,10 +24,39 @@ class InitialAttempt:
             digit = int(digits[index])
             chars = digit_to_chars[digit]
             for char in chars:
-                curr += char
-                recurse(index + 1, curr)
-                curr = curr[:-1]
+                recurse(index + 1, curr + char)
 
         recurse(0, "")
 
         return result
+
+# Time complexity: O(n * 4^n ) because we have n characters and up to 4 branches n times
+# Space complexity: O(n) extra space, O(n * 4^n) space for the output list.
+
+
+class Iteration:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        res = [""]
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        for digit in digits:
+            tmp = []
+            for curStr in res:
+                for c in digitToChar[digit]:
+                    tmp.append(curStr + c)
+            res = tmp
+        return res
+
+# same time and space complexity, this way might be slightly more readable
